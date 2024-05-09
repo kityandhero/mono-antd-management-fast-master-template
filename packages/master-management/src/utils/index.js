d28400293ac464869ee1ba0763db4153113d51e1
kityandhero <kityandhero@126.com>
2024-05-09 17:14:11 +0800

    fix(master-management): :bug: update

--- packages/master-management/src/utils/index.js
+++ packages/master-management/src/utils/index.js
@@ -1,2 +1,4 @@
+export * from './custom';
 export * from './init';
+export * from './theme';
 export * from './tools';
--- packages/master-management/src/utils/tools.js
+++ packages/master-management/src/utils/tools.js
@@ -1,4 +1,4 @@
-import { Divider, Input, Space } from 'antd';
+import { Divider, Space } from 'antd';
 
 import { getModelRemoteData } from 'easy-soft-dva';
 import {
@@ -10,35 +10,10 @@ import {
   isNull,
 } from 'easy-soft-utility';
 
-import { cardConfig, layoutCollection } from 'antd-management-fast-common';
+import { cardConfig } from 'antd-management-fast-common';
 import { buildButton, iconBuilder } from 'antd-management-fast-component';
 
 import { keyValueEditModeCollection } from '../customConfig';
-
-export const themeToken = {
-  // colorBgAppListIconHover: 'rgba(0,0,0,0.06)',
-  // colorTextAppListIconHover: 'rgba(255,255,255,0.95)',
-  // colorTextAppListIcon: 'rgba(255,255,255,0.85)',
-  sider: {
-    colorBgCollapsedButton: '#fff',
-    colorTextCollapsedButtonHover: 'rgba(0,0,0,0.65)',
-    colorTextCollapsedButton: 'rgba(0,0,0,0.45)',
-    colorMenuBackground: '#001529',
-    colorBgMenuItemCollapsedHover: 'rgba(0,0,0,0.06)',
-    colorBgMenuItemCollapsedSelected: 'rgba(0,0,0,0.15)',
-    colorBgMenuItemCollapsedElevated: 'rgba(0,0,0,0.85)',
-    colorMenuItemDivider: 'rgba(255,255,255,0.15)',
-    colorBgMenuItemHover: 'rgba(0,0,0,0.06)',
-    colorBgMenuItemSelected: '#1b90ff',
-    colorTextMenuSelected: '#fff',
-    colorTextMenuItemHover: '#fff',
-    colorTextMenu: 'rgba(255,255,255,0.75)',
-    colorTextMenuSecondary: 'rgba(255,255,255,0.65)',
-    colorTextMenuTitle: 'rgba(255,255,255,0.95)',
-    colorTextMenuActive: 'rgba(255,255,255,0.95)',
-    colorTextSubMenuSelected: '#fff',
-  },
-};
 
 export function getSexName(value) {
   let result = '未知';
@@ -102,90 +77,6 @@ export function getTitle() {
   }
 
   return shortName;
-}
-
-export function buildActionItems() {
-  return [
-    ({ layout: layoutValue }) => {
-      if (layoutValue === layoutCollection.side) {
-        return iconBuilder.search({
-          style: {
-            color: '#868686',
-          },
-        });
-      }
-      return (
-        <Input
-          style={{
-            borderRadius: 4,
-            marginInlineEnd: 12,
-            backgroundColor: 'rgba(0,0,0,0.03)',
-          }}
-          prefix={iconBuilder.search({
-            style: {
-              color: '#868686',
-            },
-          })}
-          placeholder="搜索方案"
-          variant="borderless"
-        />
-      );
-    },
-    iconBuilder.infoCircle(
-      {
-        style: {
-          color: '#868686',
-        },
-      },
-      true,
-    ),
-  ];
-}
-
-export function buildSiderMenuExtra() {
-  return (
-    <div
-      style={{
-        paddingTop: '5px',
-      }}
-    >
-      <Space align="center">
-        <Input
-          style={{
-            borderRadius: 4,
-            backgroundColor: 'rgba(0,0,0,0.03)',
-          }}
-          prefix={iconBuilder.search({
-            style: {
-              color: 'rgba(0, 0, 0, 0.15)',
-            },
-          })}
-          placeholder="搜索方案"
-          variant="borderless"
-        />
-
-        {iconBuilder.search({
-          style: {
-            color: 'var(--ant-primary-color)',
-            fontSize: 18,
-          },
-        })}
-      </Space>
-    </div>
-  );
-}
-
-export function buildSiderMenuFooter() {
-  return (
-    <div
-      style={{
-        textAlign: 'center',
-        paddingBlockStart: 12,
-      }}
-    >
-      <div>Menu Footer</div>
-    </div>
-  );
 }
 
 export function buildKeyTag(key) {
